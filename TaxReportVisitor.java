3541e478534a5ccdbc9e74feca5e3c09254ca974
Phan Hạo Lương <phanhaoluong@gmail.com>
2026-03-25 14:53:14 +0700

    Done: TaxReportVisitor

--- TaxReportVisitor.java
+++ TaxReportVisitor.java
@@ -3,21 +3,21 @@ public class TaxReportVisitor implements InstrumentVisitor {
 
 	@Override
 	public void visit(Bond bond) {
-
+		this.totalTaxLiability += bond.annualCouponPayment(1) * 0.3;
 	}
 
 	@Override
 	public void visit(Stock stock) {
-
+		this.totalTaxLiability += stock.getCurrentPriceValue() * 0.15;
 	}
 
 	@Override
 	public void visit(Future future) {
-
+		this.totalTaxLiability += future.getCurrentPriceValue() * 0.2;
 	}
 
 	@Override
 	public void visit(Option option) {
-
+		this.totalTaxLiability += option.getCurrentPriceValue() * 0.2;
 	}
 }
