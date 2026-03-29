7d07531603f5fb68d6cd555b2fc7503df3b88fa8
Phan Hạo Lương <phanhaoluong@gmail.com>
2026-03-29 15:40:28 +0700

    Group the files into problems

---
+++ Problem-1/Instrument.java
@@ -0,0 +1,69 @@
+import java.awt.List;
+import java.time.LocalDateTime;
+
+public abstract class Instrument {
+	private final String symbol;
+	private String name;
+	private double currentPrice;
+	private LocalDateTime lastUpdated;
+
+	public Instrument(String symbol, String name, double currentPrice) {
+		// TODO
+		// throw new UnsupportedOperationException("TODO");
+		this.symbol = symbol;
+		this.name = name;
+		this.currentPrice = currentPrice;
+		this.lastUpdated = LocalDateTime.now();
+	}
+
+	public abstract double riskScore();
+
+	public abstract String assetClass();
+
+	public void updatePrice(double newPrice) {
+		// TODO
+		// throw new UnsupportedOperationException("TODO");
+		if (newPrice < 0)
+			throw new IllegalArgumentException();
+		else {
+			this.currentPrice = newPrice;
+			this.lastUpdated = LocalDateTime.now();
+		}
+	}
+
+	public String getSymbol() {
+		// TODO
+		// throw new UnsupportedOperationException("TODO");
+		return this.symbol;
+	}
+
+	public String getName() {
+		// TODO
+		// throw new UnsupportedOperationException("TODO");
+		return this.name;
+	}
+
+	public double getCurrentPriceValue() {
+		// TODO
+		// throw new UnsupportedOperationException("TODO");
+		return this.currentPrice;
+	}
+
+	public LocalDateTime getLastUpdated() {
+		// TODO
+		// throw new UnsupportedOperationException("TODO");
+		return this.lastUpdated;
+	}
+
+	@Override
+	public String toString() {
+		// TODO
+		// throw new UnsupportedOperationException("TODO");
+		return String.format(
+				"%s[symbol=%s, price=%.2f, risk=%.1f]",
+				this.getClass().getSimpleName(),
+				this.getSymbol(),
+				this.getCurrentPriceValue(),
+				this.riskScore());
+	}
+}
