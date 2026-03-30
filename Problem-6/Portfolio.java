6caa113616b67cc2329e95cc3228fd24f8a4bddb
Phan Hạo Lương <phanhaoluong@gmail.com>
2026-03-30 23:00:34 +0700

    Fix: Portfolio.removePosition() returning after just 1 position

--- Problem-6/Portfolio.java
+++ Problem-6/Portfolio.java
@@ -29,11 +29,12 @@ public class Portfolio implements Observable<String> {
 		// TODO
 		// throw new UnsupportedOperationException("TODO");
 		for (Position p : this.positions) {
-			if (p.getInstrument().getSymbol() == symbol)
+			if (p.getInstrument().getSymbol() == symbol) {
 				this.positions.remove(p);
-			String noti = "REMOVED: " + symbol;
-			this.notifyObservers(noti);
-			return;
+				String noti = "REMOVED: " + symbol;
+				this.notifyObservers(noti);
+				return;
+			}
 		}
 
 		throw new PositionNotFoundException(String.format("Position not found: %s", symbol));
